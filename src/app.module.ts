4557723d796c4682b4a84cf94c46565d95e2d006
alfredaffia <alfredaffia478@gmail.com>
2025-05-13 14:51:36 +0100

    category deleted

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
 import { PostModule } from './post/post.module';
 import { CommentModule } from './comment/comment.module';
-import { CategoryModule } from './category/category.module';
 import { AuthModule } from './auth/auth.module';
 
 @Module({
