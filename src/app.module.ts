abe84e998954762a1f6e8da435a53d957901b6c3
alfredaffia <alfredaffia478@gmail.com>
2025-05-13 14:47:44 +0100

    resource files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,12 @@
 import { Module } from '@nestjs/common';
+import { UserModule } from './user/user.module';
+import { PostModule } from './post/post.module';
+import { CommentModule } from './comment/comment.module';
+import { CategoryModule } from './category/category.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
+  imports: [UserModule, PostModule, CommentModule, CategoryModule, AuthModule],
   controllers: [],
   providers: [],
 })
