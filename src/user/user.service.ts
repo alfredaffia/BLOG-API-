cf5fe0ac21d522bcf77808819a0685a7181f7d5d
alfredaffia <alfredaffia478@gmail.com>
2025-05-14 17:52:30 +0100

    signup

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,7 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { User } from './entities/user.entity';
+import * as argon2 from 'argon2';
 import { InjectRepository } from '@nestjs/typeorm'
 import { Repository } from 'typeorm';
 import { JwtService } from '@nestjs/jwt';
@@ -13,11 +14,30 @@ export class UserService {
     @InjectRepository(User)
     private userRepository: Repository<User>,
     private jwtService: JwtService
-  ) { }
+  ) { } 
 
   async create(payload: CreateUserDto) {
     const { email, userName, password, ...rest } = payload;
     payload.email = payload.email.toLowerCase()
+    const user = await this.userRepository.findOne({ where: { email:email } });
+    if(user){
+      throw new ConflictException('user alredy exists')
+    }
+    const hashPassword =await argon2.hash(password)
+
+    const userDetails = await this.userRepository.save({
+       email,
+       password: hashPassword,
+       userName,
+      ...rest
+    })
+const userPayload ={id:userDetails.id, email:userDetails.email}
+return{
+      userId: userDetails.id,
+      userName: userDetails.userName,
+      userEmail: userDetails.email,
+  access_token: await this.jwtService.signAsync(userPayload),
+}
   }
 
   findAll() {
