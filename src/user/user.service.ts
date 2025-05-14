b5528aa77e3703e7b56a6aa8ff9f8348641c8727
alfredaffia <alfredaffia478@gmail.com>
2025-05-14 18:06:17 +0100

    signin

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,11 +1,13 @@
-import { ConflictException, Injectable } from '@nestjs/common';
+import { ConflictException, HttpException, Injectable, Res } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { User } from './entities/user.entity';
+import {  Response } from 'express';
 import * as argon2 from 'argon2';
 import { InjectRepository } from '@nestjs/typeorm'
 import { Repository } from 'typeorm';
 import { JwtService } from '@nestjs/jwt';
+import { LoginDto } from './dto/login.dto';
 
 
 @Injectable()
@@ -39,6 +41,50 @@ return{
   access_token: await this.jwtService.signAsync(userPayload),
 }
   }
+
+
+    async verifyPassword(hashedPassword: string, plainPassword: string,): Promise<boolean> {
+    try {
+      return await argon2.verify(hashedPassword, plainPassword);
+    } catch (err) {
+      console.log(err.message)
+      return false;
+    }
+  }
+    async signIn(payload: LoginDto, @Res() res: Response) {
+    const { email, password } = payload;
+    // const user = await this.userRepo.findOne({where:{email:email}  })
+    const user = await this.userRepository.createQueryBuilder("user").addSelect("user.password").where("user.email = :email", { email: payload.email }).getOne()
+
+    // console.log('Fetched User:', user);
+
+    if (!user) {
+      throw new HttpException('No email found', 400)
+    }
+    // if (user.IsBlocked === true) {
+    //   throw new ForbiddenException('your Accoount have been blocked' );
+    // }
+    const checkedPassword = await this.verifyPassword(user.password, password);
+    if (!checkedPassword) {
+      throw new HttpException('password incorrect', 400)
+    }
+    const token = await this.jwtService.signAsync({
+      email: user.email,
+      id: user.id
+    });
+
+    res.cookie('isAuthenticated', token, {
+      httpOnly: true,
+      maxAge: 1 * 60 * 60 * 1000
+    });
+    // delete user.password
+    return res.send({
+      success: true,
+      userToken: token
+
+    })
+  }
+
 
   findAll() {
     return `This action returns all user`;
