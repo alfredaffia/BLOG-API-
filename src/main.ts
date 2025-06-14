88e1f6f566237fe051774b368023a1bed12a4ee3
alfredaffia <alfredaffia478@gmail.com>
2025-06-14 16:05:52 +0100

    port

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { Console } from 'console';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -10,5 +11,6 @@ async function bootstrap() {
     forbidNonWhitelisted: true
   }))
   await app.listen(process.env.PORT ?? 3000);
+  console.log(`server listening on port ${process.env.PORT ?? 3000}`)
 }
 bootstrap();
